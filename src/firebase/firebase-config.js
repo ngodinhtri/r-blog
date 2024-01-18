26b62a1c38c586d960ec72e36f3e9ea2bc656728
ngodi00 <ngodinhtri@outlook.com>
2024-01-18 13:51:39 +0700

    :wrench: setup firebase

---
+++ src/firebase/firebase-config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAf0kl-nEESd0b7yFb8wyIJxc-MrdvemAc",
+  authDomain: "r-bl-f24d2.firebaseapp.com",
+  projectId: "r-bl-f24d2",
+  storageBucket: "r-bl-f24d2.appspot.com",
+  messagingSenderId: "35781779968",
+  appId: "1:35781779968:web:badac3a8eb50f8b7586ee5",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
+export const auth = getAuth(app);
