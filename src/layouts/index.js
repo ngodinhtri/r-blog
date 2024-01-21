8fef1b028ed9ae171bc529a96f2d23424d5a2b39
ngodi00 <ngodinhtri@outlook.com>
2024-01-21 10:25:05 +0700

    :ambulance: fix import not default

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,5 +1,6 @@
 import { AuthPageLayout } from "@/layouts/AuthPageLayout.jsx";
 import { Header } from "@/layouts/Header.jsx";
 import { MainLayout } from "@/layouts/MainLayout.jsx";
+import { ManagePagesLayout } from "@/layouts/ManagePagesLayout.jsx";
 
-export { AuthPageLayout, Header, MainLayout };
+export { AuthPageLayout, Header, MainLayout, ManagePagesLayout };
