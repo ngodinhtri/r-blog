9596ef9c0f4feb26f713432662020d8c6549360d
ngodi00 <ngodinhtri@outlook.com>
2024-01-21 23:14:48 +0700

    :card_file_box: (Firebase) upload image to cloud

---
+++ src/firebase/uploadImageToFirebase.js
@@ -0,0 +1,41 @@
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+import { toast } from "react-toastify";
+
+export default function uploadImageToFirebase(file) {
+  const storage = getStorage();
+  // Upload file and metadata to the object 'images/mountains.jpg'
+  const storageRef = ref(storage, "images/" + file.name);
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  // Listen for state changes, errors, and completion of the upload.
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      switch (snapshot.state) {
+        case "paused":
+          console.log("Upload is paused");
+          break;
+        case "running":
+          console.log("Upload is running");
+          break;
+      }
+    },
+    (error) => {
+      // A full list of error codes is available at
+      // https://firebase.google.com/docs/storage/web/handle-errors
+      toast(error.message);
+    },
+    () => {
+      // Upload completed successfully, now we can get the download URL
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        console.log("File available at", downloadURL);
+        return downloadURL;
+      });
+    },
+  );
+}
